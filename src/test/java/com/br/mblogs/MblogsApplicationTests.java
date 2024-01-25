25e2ee5f9a90c91f6a3caadfc1e65ee13b24fe68
dudis <137111775+darknessstonight@users.noreply.github.com>
2024-01-25 11:07:47 -0300

    Add files via upload

---
+++ src/test/java/com/br/mblogs/MblogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mblogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MblogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
